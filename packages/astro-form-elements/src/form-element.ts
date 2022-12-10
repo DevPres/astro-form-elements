fe55701d8febcb995bed935431e7a292525f6d50
Dario Presutti <dariopresdev@protonmail.com>
2022-12-10 19:11:51 +0100

    create logic for registering element | changed rule about emit value

--- packages/astro-form-elements/src/form-element.ts
+++ packages/astro-form-elements/src/form-element.ts
@@ -1,78 +1,117 @@
 import { Observable } from "rxjs";
-import { ElementChanges, IElementChanges } from "./element-changes";
+import { AbstractElement } from "./abstract-element";
+import {
+  ElementChangesEvent,
+  ElementChangesEventType,
+  ElementChangesValue,
+} from "./element-changes";
+
+export interface ElementOptions {
+  elementName: string;
+}
 
 export default class FormElement {
   constructor(formElementName: string) {
-    let inputRef = document.querySelector(
-      `input[data-formElementName="${formElementName}"]`
-    ) as HTMLInputElement;
-    if (!!inputRef) {
-      this.element = inputRef;
-      this._value = inputRef.value;
-      this._data = inputRef.dataset;
-      this.element.addEventListener("input", this._onChange.bind(this));
-      this.element.addEventListener("focus", this._onFocus.bind(this));
+    let ref = document.querySelector(
+      `[data-formElementName="${formElementName}"]`
+    );
+
+    try {
+      this._registerElement(ref, {
+        elementName: formElementName,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+
+    if (ref instanceof HTMLInputElement) {
+      this._element = ref;
+      this._value = ref.value;
+      this._data = ref.dataset;
+      this._element.addEventListener("input", this._onChange.bind(this));
+      this._element.addEventListener("focus", this._onFocus.bind(this));
+      this._element.addEventListener("blur", this._onBlur.bind(this));
     }
   }
 
-  private element: HTMLInputElement;
+  private _element: HTMLInputElement;
   private _value: any;
-  private _lastInsert: string;
+  private _lastValueInsert: string;
+  private _lastEvent: ElementChangesEventType;
+  private _elementName: string;
   private _data: any;
   private _touched = false;
-  private _elementChanges$ = new ElementChanges();
+  private _elementChanges$ = new ElementChangesEvent();
 
-  private _propagateChanges() {
+  private _elementChanges() {
     this._elementChanges$.next({
-      value: this._value,
-      lastInsert: this._lastInsert,
+      type: this._lastEvent,
+      element: {
+        name: this._elementName,
+      },
+      data: {
+        value: this._value,
+        lastValueInsert: this._lastValueInsert,
+      },
     });
   }
 
   private _onChange(event: InputEvent): void {
-    //console.log(event);
+    console.log(event);
     const target = event.target as HTMLInputElement;
-    this._lastInsert = event.data as string;
+    this._lastValueInsert = event.data as string;
     this._value = target.value;
-    this._propagateChanges();
-    return;
+    this._lastEvent = event.type as ElementChangesEventType;
+    this._elementChanges();
   }
 
   private _onFocus(event: InputEvent): void {
-    //console.log(event);
-    this._touched = true;
-    this._propagateChanges();
-    return;
+    this._lastEvent = event.type as ElementChangesEventType;
+
+    if (!this._touched) {
+      this._touched = true;
+      this._elementChanges();
+    }
   }
 
-  valueChanges(): Observable<IElementChanges> {
+  private _onBlur(event: InputEvent): void {
+    this._lastEvent = event.type as ElementChangesEventType;
+
+    if (!this._touched) {
+      this._touched = true;
+      this._elementChanges();
+    }
+  }
+
+  private _registerElement(
+    ref: Element | null,
+    elementOptions: ElementOptions
+  ): void {
+    if (ref === null) {
+      throw new ReferenceError(
+        `A formElement with name '${elementOptions.elementName} was not found'`
+      );
+    }
+    if (ref instanceof HTMLInputElement) {
+      this._element = ref;
+      this._elementName = elementOptions.elementName;
+      return;
+    }
+    throw new TypeError(
+      `The Element with name '${elementOptions.elementName}' has to extend the CustomElement interface in a way to be sync
+      
+      ex:
+      \`\`\`
+      class CustomInput extends CustomElement {
+        ...
+      }
+
+      The class will be not instanciate.
+      `
+    );
+  }
+
+  valueChanges(): Observable<ElementChangesValue> {
     return this._elementChanges$.asObservable();
   }
 }
-
-/* customElements.define("form-element", FormElement); */
-
-/* 
-const subscribers = [];
-
-const subscribe = callback => {
-  subscribers.push(callback);
-};
-
-const unsubscribe = callback => {
-  const index = subscribers.indexOf(callback);
-  if (index !== -1) {
-    subscribers.splice(index, 1);
-  }
-};
-
-const publish = value => {
-  subscribers.forEach(callback => callback(value));
-};
-
-// Later, when you want to emit a value to the subscribers, you can call the
-// `publish` function with the value.
-publish();
-
-
-*/
