cd69a9f9025ac2d85b9df47c7e81e001293fa50e
Dario Presutti <dariopresdev@protonmail.com>
2022-12-13 14:36:39 +0100

    create service

--- packages/astro-form-elements/src/form-service.ts
+++ packages/astro-form-elements/src/form-service.ts
@@ -5,14 +5,44 @@ class FormService {
     console.log("hello service");
   }
 
-  #elementRegistry = new Map();
+  #elementRegistry = new Map<string, FormElement>();
 
   registerElement(element: FormElement) {
-    this.#elementRegistry.set(
-      element.attributes.getNamedItem("formElementName")!.value,
-      element
-    );
+    let key = this.getElementKey(element);
+    let exist = this.checkIfElementAlreadyExist(key);
+
+    this.#elementRegistry.set(key, element);
+  }
+
+  private getElementKey(element: FormElement): string {
+    let elementNameData = element.attributes.getNamedItem("formElementName");
+    let elementIdData = element.attributes.getNamedItem("formElementId");
+    if (!!elementNameData) {
+      return elementNameData.value;
+    }
+    /**
+     * At least one of the attributes is defined, because the constructor of
+     * FormElement throw Error instad
+     */
+    return elementIdData!.value;
+  }
+
+  private checkIfElementAlreadyExist(key: string) {
+    return !!this.get(key);
+  }
+
+  /**
+   * Get the Element, if this is registered, or throw an Error instad
+   *
+   * @param key is the formElementName or formElement id of the FormElement to search
+   */
+  get(key: string): FormElement | undefined {
+    let element = this.#elementRegistry.get(key);
+    if (!!element) {
+      return element;
+    }
+    return;
   }
 }
 
-export const formService = new FormService();
+export default new FormService();
