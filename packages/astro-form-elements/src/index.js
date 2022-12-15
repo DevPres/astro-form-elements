5ebbbaac16487ee374206095f5c8d96c040b3b4d
Dario Presutti <dariopresdev@protonmail.com>
2022-12-15 15:44:38 +0100

    exported base checkbox

--- packages/astro-form-elements/src/index.js
+++ packages/astro-form-elements/src/index.js
@@ -1,3 +1,4 @@
 export { default as BaseInput } from "./BaseInput.astro";
+export { default as BaseCheckbox } from "./BaseCheckbox.astro";
 export { default as FormElement } from "./form-element";
 export { default as formService } from "./form-service";
