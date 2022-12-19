828e4d857e280b4ff8d7f001fa84f02494a2a995
Dario Presutti <dariopresdev@protonmail.com>
2022-12-19 16:37:57 +0100

    used FromElement.astro

--- packages/astro-form-elements/components/index.ts
+++ packages/astro-form-elements/components/index.ts
@@ -1,3 +1,3 @@
 export { default as BaseInput } from "./BaseInput.astro";
-
 export { default as BaseCheckbox } from "./BaseCheckbox.astro";
+export { default as FormElement } from "./FormElement.astro";
