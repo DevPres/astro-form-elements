bfe08fdf7929c9b27ed87d70c6b6a419a9f84552
Dario Presutti <dariopresdev@protonmail.com>
2022-12-19 16:36:57 +0100

    lock + scaffolding

--- packages/astro-form-elements/index.ts
+++ packages/astro-form-elements/index.ts
@@ -1,2 +1,2 @@
 export { default as FormElements } from "./form-elements-service";
-export { default as FormElement } from "./form-element";
+export { default as FormElement } from "./components/form-element";
