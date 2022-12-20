b751d84e1fb8750cc97ddf502c433dd32ac56d06
Dario Presutti <dariopresdev@protonmail.com>
2022-12-20 19:35:24 +0100

    exportedd previous component

--- packages/astro-form-elements/components/index.ts
+++ packages/astro-form-elements/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Input } from "./Input.astro";
+export { default as Form } from "./Form.astro";
 export { default as FormElement } from "./FormElement.astro";
+export { default as FormGroupElement } from "./FormGroupElement.astro";
