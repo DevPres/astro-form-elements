c644ae2af6020c9527b21ba1673e2f050495f040
Dario Presutti <dariopresdev@protonmail.com>
2022-12-20 19:33:09 +0100

    exported formGrooupElement

--- packages/astro-form-elements/index.ts
+++ packages/astro-form-elements/index.ts
@@ -1,2 +1,3 @@
 export { default as FormElementsRegistry } from "./form-elements-registry";
 export { default as FormElement } from "./form-element";
+export { default as FormGroupElement } from "./form-element";
